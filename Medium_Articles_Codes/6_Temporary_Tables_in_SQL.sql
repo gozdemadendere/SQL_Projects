e1f37ce03df2caea1a575bebb36321dc07f1c08c
Gözde Barın <90986708+gozdebarin@users.noreply.github.com>
2023-06-25 17:33:51 +0200

    Add files via upload

---
+++ Medium_Articles_Codes/6_Temporary_Tables_in_SQL.sql
@@ -0,0 +1,75 @@
+
+
+
+-- I'd like to show this query's result as a temporary table.
+-- (It shows book's titles and publisher's country.)
+
+SELECT title, country
+FROM titles
+LEFT JOIN publishers
+USING (pub_id);
+
+
+
+
+-- Let's create a temporary table as 'titles_publishers'.
+
+CREATE TEMPORARY TABLE titles_publishers
+SELECT title, country
+FROM titles
+LEFT JOIN publishers
+USING (pub_id);
+
+-- Let's retrieve all the data from the temporary table 'titles_publishers'.
+
+SELECT *
+FROM titles_publishers;
+
+
+
+
+-- Display book's titles and publisher's country where title starts with 'The'.
+
+SELECT *
+FROM titles_publishers
+WHERE title LIKE 'The%';
+
+
+
+
+-- Display book's titles and publisher's country where title starts with 'The'.
+
+SELECT titles_publishers.title, titles.price
+FROM titles_publishers
+LEFT JOIN titles
+ON titles_publishers.title = titles.title;
+
+
+
+
+DROP TABLE titles_publishers;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
